5e2557996d57e97de1a77fe06dfcada8342d551b
lixianfa <lixianfa.official@gmail.com>
2020-03-28 01:36:08 +0800

    kern_levels linux系统信息打印级别
    一共有1-7级，分别对应不同的信息

--- include/linux/kern_levels.h
+++ include/linux/kern_levels.h
@@ -5,13 +5,21 @@
 #define KERN_SOH	"\001"		/* ASCII Start Of Header */
 #define KERN_SOH_ASCII	'\001'
 
+//紧急消息，常常是那些崩溃前的消息
 #define KERN_EMERG	KERN_SOH "0"	/* system is unusable */
+//需要立即采取动作的情形
 #define KERN_ALERT	KERN_SOH "1"	/* action must be taken immediately */
+//严重情况，常常与严重的硬件或者软件失效有关
 #define KERN_CRIT	KERN_SOH "2"	/* critical conditions */
+//用来报告错误情况，设备驱动常常使用来报告硬件故障
 #define KERN_ERR	KERN_SOH "3"	/* error conditions */
+//有问题的情况的警告，这些情况自己不会引起系统的严重问题
 #define KERN_WARNING	KERN_SOH "4"	/* warning conditions */
+//正常情况，但是仍然值得注意。在这个级别一些安全相关的情况会报告。
 #define KERN_NOTICE	KERN_SOH "5"	/* normal but significant condition */
+//信息型消息，在这个级别，很多驱动在启动时打印它们发现的硬件的信息
 #define KERN_INFO	KERN_SOH "6"	/* informational */
+//调试消息
 #define KERN_DEBUG	KERN_SOH "7"	/* debug-level messages */
 
 #define KERN_DEFAULT	""		/* the default kernel loglevel */
@@ -26,6 +34,7 @@
 /* integer equivalents of KERN_<LEVEL> */
 #define LOGLEVEL_SCHED		-2	/* Deferred messages from sched code
 					 * are set to this special level */
+//可以通过过/proc/sys/kernel/printk修改日志打打印级别
 #define LOGLEVEL_DEFAULT	-1	/* default (or last) loglevel */
 #define LOGLEVEL_EMERG		0	/* system is unusable */
 #define LOGLEVEL_ALERT		1	/* action must be taken immediately */
